e7d4b59e81961d67b5c277e5710be984aebecc9b
cryptus-neoxys <sharma.dev4242@gmail.com>
2021-08-05 01:57:10 +0530

    fix 🐛: (utils/search.js) match parameters + typo

--- utils/es/search.js
+++ utils/es/search.js
@@ -1,18 +1,17 @@
 const { Client } = require("@elastic/elasticsearch");
 const client = new Client({ node: "http://elasticsearch:9200" });
 
-async function run(searchQuery, pageSize, pageNumber) {
+async function runSearch(searchQuery, pageNumber, pageSize) {
   // Let's search!
   const { body } = await client.search({
     index: "videos",
     from: pageSize * (pageNumber - 1),
     size: pageSize,
-    sort: [{ publishedAt: "desc" }],
+    sort: "publishedAt:desc",
     body: {
       query: {
         match: {
           title: searchQuery,
-          description: searchQuery,
         },
       },
     },
@@ -21,3 +20,7 @@ async function run(searchQuery, pageSize, pageNumber) {
   console.log(body.hits.hits);
   return body.hits.hits;
 }
+
+module.exports = {
+  runSearch,
+};
