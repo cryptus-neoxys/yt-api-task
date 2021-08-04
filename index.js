0eecdf50798c3b0f35d0524799b92af22fc39a77
cryptus-neoxys <sharma.dev4242@gmail.com>
2021-08-04 15:46:11 +0530

    feat ✨: Dockerize application

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4000;
 
 const app = express();
 
 app.use(cors());
+
+app.get("/api", (req, res) => {
+  res.send({ message: "Hi 👋🏻" });
+});
 
 app.listen(PORT, () => console.log(`listening on http://localhost:${PORT}`));
